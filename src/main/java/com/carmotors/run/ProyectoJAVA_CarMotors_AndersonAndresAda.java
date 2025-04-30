616e3958b2f62cdfebaaad2e2199015fbcdef619
AndresPortilla <andresportilla@gmail.com>
2025-04-29 19:43:01 -0500

    Initial project structure

---
+++ src/main/java/com/carmotors/run/ProyectoJAVA_CarMotors_AndersonAndresAda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.carmotors.run;
+
+/**
+ *
+ * @author ANDRES
+ */
+public class ProyectoJAVA_CarMotors_AndersonAndresAda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
